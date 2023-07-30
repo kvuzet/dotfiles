9e7b24d31833f099ea8151676bdf97f07e6d3def
Erin C <kvuzet@cotse.net>
2023-07-29 21:40:51 -0700

    quick bundle script and a gitignore

---
+++ vim/bundle.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/bash
+
+cd ~/.vim/bundle
+
+git clone https://github.com/mileszs/ack.vim.git
+git clone https://github.com/dense-analysis/ale.git
+git clone https://github.com/jiangmiao/auto-pairs.git
+git clone https://github.com/mattn/emmet-vim.git
+git clone https://github.com/junegunn/fzf.vim.git
+git clone https://github.com/junegunn/fzf.git
+git clone https://github.com/preservim/nerdtree.git
+git clone https://github.com/haishanh/night-owl.vim.git
+git clone https://github.com/tomtom/tcomment_vim.git
+git clone https://github.com/tpope/vim-dispatch.git
+git clone https://github.com/tpope/vim-endwise.git
+git clone https://github.com/sheerun/vim-polyglot.git
