509e37376714a61298de38998fa25d082ddcf191
Erin C <kvuzet@cotse.net>
2023-10-25 13:14:01 -0700

    updates based on moving into a new computer

--- vim/bundle.sh
+++ vim/bundle.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/bash
 
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 cd ~/.vim/bundle
 
 git clone https://github.com/mileszs/ack.vim.git
--- zsh/base.zsh
+++ zsh/base.zsh
@@ -17,12 +17,13 @@ alias t="todo-txt"
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
 alias ssh='TERM=xterm-256color ssh'
+alias python="python3"
 
 # PATH
 PATH=${PATH}:"/snap/bin:${HOME}/.local/bin"
 export GPG_TTY=$(tty)
 
 #export TERM=xterm-256color
-
+export EDITOR="vim"
 # powerline
 eval "$(powerline-daemon -q)"
